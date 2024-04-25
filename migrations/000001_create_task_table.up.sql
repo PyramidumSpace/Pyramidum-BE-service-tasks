20f5a69d7f3db8c10ac86d0bd23150001ac35731
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2024-04-25 21:01:37 +0700

    CONTE-7: Created task migrations.

---
+++ migrations/000001_create_task_table.up.sql
@@ -0,0 +1,23 @@
+CREATE TYPE progress_status AS ENUM(
+    'canceled',
+    'in progress',
+    'done'
+);
+
+CREATE TABLE task (
+    id UUID UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    header TEXT NOT NULL,
+    text TEXT NOT NULL,
+    deadline TIMESTAMP NOT NULL,
+    progress_status progress_status NOT NULL,
+    is_urgent BOOLEAN NOT NULL,
+    is_important BOOLEAN NOT NULL,
+    owner_id UUID NOT NULL,
+    parent_task_id UUID,
+    possible_deadline TIMESTAMP NOT NULL,
+    weight INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (parent_task_id) REFERENCES task(id)
+)
