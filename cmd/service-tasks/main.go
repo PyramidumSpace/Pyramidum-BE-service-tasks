1fa9bb4f56833f87d6cd20d5cbf6706322f0a395
fatalistix <slavabal11@outlook.com>
2024-05-14 19:05:19 +0700

    CONTE-11: log creation, app startup, gracefull shutdown

--- cmd/service-tasks/main.go
+++ cmd/service-tasks/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"github.com/g-vinokurov/pyramidum-backend-service-tasks/internal/app"
 	"github.com/g-vinokurov/pyramidum-backend-service-tasks/internal/config"
 	"github.com/g-vinokurov/pyramidum-backend-service-tasks/internal/env"
+	slogattr "github.com/g-vinokurov/pyramidum-backend-service-tasks/internal/lib/log/slog/attr"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	log := slog.Default()
+
 	// init environment variables from .env file
 	env.MustLoadEnv()
 
+	log.Info("environment variables loaded")
+
 	configPath := os.Getenv("CONFIG_PATH")
+
 	cfg := config.MustLoadConfig(configPath)
 
-	slog.Info("config loaded", slog.Any("config", cfg))
+	log.Info("config loaded", slog.Any("config", cfg))
+
+	a, err := app.NewApp(log, cfg)
+	if err != nil {
+		log.Error("cannot create app", slogattr.Err(err))
+		os.Exit(1)
+	}
+
+	go func() {
+		if err := a.Run(); err != nil {
+			log.Error("cannot run app", slogattr.Err(err))
+			os.Exit(1)
+		}
+	}()
+
+	log.Info("app started")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	log.Info("stopping app")
+
+	a.Stop()
+
+	log.Info("app stopped")
 }
