a48e3db9df9e775f376a18a923df50a52186fdd7
fatalistix <slavabal11@outlook.com>
2024-05-14 19:09:41 +0700

    CONTE-11: removed gorm, added sql from sdk

--- internal/database/connection/postgres/connection.go
+++ internal/database/connection/postgres/connection.go
@@ -1,31 +1,30 @@
 package postgres
 
 import (
+	"database/sql"
 	"fmt"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
-type sslMode string
+type Database struct {
+	db *sql.DB
+}
 
-const (
-	SSLDisable    sslMode = "disable"
-	SSLRequire    sslMode = "require"
-	SSLVerifyFull sslMode = "verify-full"
-	SSLVerifyCa   sslMode = "verify-ca"
-)
-
-func NewDB(host string, port uint16, user string, password string, dbname string, sslMode sslMode) (*gorm.DB, error) {
+func NewDatabase(host string, port uint16, user string, password string, dbname string, sslMode string) (*Database, error) {
 	const op = "database.connection.postgres.NewDB"
 
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
 		host, user, password, dbname, port, sslMode,
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	return db, nil
+
+	return &Database{db: db}, nil
+}
+
+func (d *Database) DB() *sql.DB {
+	return d.db
 }
