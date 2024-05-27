ed3d54eb9de71421396b9a97a14d45cf25ef3ad2
fatalistix <slavabal11@outlook.com>
2024-05-27 17:00:57 +0700

    CONTE-18: added some mappers

--- internal/domain/model/task.go
+++ internal/domain/model/task.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"time"
 )
@@ -10,12 +11,33 @@ type Task struct {
 	Header           string
 	Text             string
 	Deadline         time.Time
-	ProgressStatus   string
+	ProgressStatus   ProgressStatus
 	IsUrgent         bool
 	IsImportant      bool
 	OwnerId          int32
-	ParentId         uuid.UUID
+	ParentId         *uuid.UUID
 	PossibleDeadline time.Time
 	ExternalImages   []string
 	Weight           int32
+}
+
+type ProgressStatus string
+
+const (
+	ProgressStatusInProgress ProgressStatus = "in progress"
+	ProgressStatusCanceled   ProgressStatus = "canceled"
+	ProgressStatusDone       ProgressStatus = "done"
+)
+
+func ProgressStatusFromString(s string) (ProgressStatus, error) {
+	switch s {
+	case "in progress":
+		return ProgressStatusInProgress, nil
+	case "canceled":
+		return ProgressStatusCanceled, nil
+	case "done":
+		return ProgressStatusDone, nil
+	default:
+		return "", fmt.Errorf("unknown progress status: %s", s)
+	}
 }
